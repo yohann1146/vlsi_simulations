c4fe5cf0201da78b719d6b905ca6da30ee0058bf
Yohann <f20231146@hyderabad.bits-pilani.ac.in>
2025-08-15 20:29:51 +0530

    Update I-V Characteristics.py

--- MOSFET/I-V Characteristics.py
+++ MOSFET/I-V Characteristics.py
@@ -1,22 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-Vgs_vals = [0.2,0.4,0.5,0.6,0.7,0.8,0.9,1]      //Test values for Gate-Source Voltage
+Vgs_vals = [0.2,0.4,0.5,0.6,0.7,0.8,0.9,1]      #Test values for Gate-Source Voltage
 Vt = 0.2      #(V) Threshold Voltage
 Bn = 1        #(ma/V^2) MOSFET Gain = u_n * C_ox * W/L
 
-Vds_vals = np.linspace(0, 1, 100)    //Vds positive for NMOS
+Vds_vals = np.linspace(0, 1, 100)    #Vds positive for NMOS
 
 def linear_id(Vov, Vds):
-  return Bn*(Vov*Vds - (Vds**2)/2)    //Eqn in Triode mode
+  return Bn*(Vov*Vds - (Vds**2)/2)    #Eqn in Triode mode
 
 def satn_id(Vov):
-  return Bn/2*Vov**2                  //Eqn in Saturation Mode
+  return Bn/2*Vov**2                  #Eqn in Saturation Mode
 
 for Vgs in Vgs_vals:
-  Vov = Vgs-Vt      //Overdrive Voltage
+  Vov = Vgs-Vt      #Overdrive Voltage
 
-  Id_vals = [linear_id(Vov, Vds) if Vds < Vov else satn_id(Vov) for Vds in Vds_vals]        //If Vov < Vds, triode mode. If Vov >= Vds, saturation.
+  Id_vals = [linear_id(Vov, Vds) if Vds < Vov else satn_id(Vov) for Vds in Vds_vals]        #If Vov < Vds, triode mode. If Vov >= Vds, saturation.
   #all Ids are in mA since Bn=1mA/V^2
 
   plt.plot(Vds_vals, Id_vals, label=f'Vgs={Vgs} V')
